0e59d68b5c0a1eb08a01bc5badf5c5b6f59368c1
toasted-coder <christmanholmes21@gmail.com>
2021-03-23 13:49:04 -0700

    fixed typo in product.js

--- models/Product.js
+++ models/Product.js
@@ -29,7 +29,7 @@ Product.init(
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      defaultValue: '10',
+      defaultValue: 10,
       validate: {
         isNumeric: true,
       },
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,12 +5,16 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
+  try {
+    
+  };
   // find all products
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
+
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
